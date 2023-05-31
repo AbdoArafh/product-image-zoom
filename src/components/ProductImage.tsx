11e8c5f14be314a17e808cd79866bc5975c800a7
AbdoArafh <abdoarafh54@gmail.com>
2023-05-31 11:47:23 +0300

    cleanup: removed some unused code

--- src/components/ProductImage.tsx
+++ src/components/ProductImage.tsx
@@ -1,5 +1,5 @@
 import { HTMLAttributes } from "preact/compat";
-import { useEffect, useRef, useState } from "preact/hooks";
+import { useEffect, useRef } from "preact/hooks";
 import styles from "./ProductImage.module.css";
 import { constrain } from "../lib/utils";
 
@@ -7,59 +7,53 @@ export default function ProductImage(props: HTMLAttributes<HTMLImageElement>) {
   const wrapperRef = useRef<HTMLDivElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const imageRef = useRef<HTMLImageElement>(null);
-  const [size, setSize] = useState<{ width: number; height: number }>();
-
-  const startTrackingMouse = (
-    ctx: CanvasRenderingContext2D | null,
-    img: HTMLImageElement | null
-  ) => {
-    if (!wrapperRef.current) return;
-    const wrapper = wrapperRef.current;
-    wrapper.addEventListener("mousemove", (e) => {
-      if (ctx && img) {
-        const size = { width: img?.offsetWidth!, height: img?.offsetHeight! };
-
-        const showRect = {
-          width: size.width / 2.5,
-          height: size.height / 2.5,
-        };
-
-        ctx.clearRect(0, 0, size.width, size.height);
-
-        const sx = constrain(
-          e.offsetX - showRect.width / 2,
-          0,
-          size.width - showRect.width
-        );
-
-        const sy = constrain(
-          e.offsetY - showRect.height / 2,
-          0,
-          size.height - showRect.height
-        );
-
-        const sw = showRect.width;
-
-        const sh = showRect.height;
-
-        ctx.drawImage(img, sx, sy, sw, sh, 0, 0, size?.width, size?.height);
-      }
-    });
-  };
 
   useEffect(() => {
-    if (wrapperRef.current) {
-      setSize({
-        width: wrapperRef.current.offsetWidth,
-        height: wrapperRef.current.offsetHeight,
-      });
-    }
-
-    if (canvasRef.current && imageRef.current) {
+    if (wrapperRef.current && canvasRef.current && imageRef.current) {
+      canvasRef.current.width = imageRef.current.offsetWidth;
+      canvasRef.current.height = imageRef.current.offsetHeight;
       const ctx = canvasRef.current.getContext("2d");
       const img = imageRef.current;
-      img.onload = () => {
-        startTrackingMouse(ctx, imageRef.current);
+
+      const handleMouseMove = (e: MouseEvent) => {
+        if (ctx && img) {
+          const size = { width: img?.offsetWidth!, height: img?.offsetHeight! };
+
+          const showRect = {
+            width: size.width / 2.5,
+            height: size.height / 2.5,
+          };
+
+          ctx.clearRect(0, 0, size.width, size.height);
+
+          const sx = constrain(
+            e.offsetX - showRect.width / 2,
+            0,
+            size.width - showRect.width
+          );
+
+          const sy = constrain(
+            e.offsetY - showRect.height / 2,
+            0,
+            size.height - showRect.height
+          );
+
+          const sw = showRect.width;
+
+          const sh = showRect.height;
+
+          ctx.drawImage(img, sx, sy, sw, sh, 0, 0, size?.width, size?.height);
+        }
+      };
+
+      const handleImageLoad = () =>
+        wrapperRef.current?.addEventListener("mousemove", handleMouseMove);
+
+      img.addEventListener("load", handleImageLoad);
+
+      return () => {
+        img.removeEventListener("load", handleImageLoad);
+        wrapperRef.current?.removeEventListener("mousemove", handleMouseMove);
       };
     }
   }, []);
@@ -68,12 +62,7 @@ export default function ProductImage(props: HTMLAttributes<HTMLImageElement>) {
     <>
       <div class={styles["image-wrapper"]} ref={wrapperRef}>
         <img {...props} ref={imageRef} />
-        <canvas
-          className={styles["canvas"]}
-          width={size?.width}
-          height={size?.height}
-          ref={canvasRef}
-        />
+        <canvas className={styles["canvas"]} ref={canvasRef} />
       </div>
     </>
   );
